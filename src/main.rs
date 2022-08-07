d6c19019be7f11f2e1278235362caa0d6dceb62f
Manoel <manoel.lobo@gmail.com>
2022-08-07 02:25:55 -0300

    feat: read io (stdin)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+    println!("Type our guess");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read input");
+
+    println!("Your guess is {guess}")
 }
